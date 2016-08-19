261d00e1e4b9901282663ffa82dae30d14ea6269
safrinafanfei <feif1021@gmail.com>
2016-08-18 23:10:58 -0400

    add one more map

--- js/landlinejs.js
+++ js/landlinejs.js
@@ -81,6 +81,17 @@ var census =  {
 
 $(function() {
 
+        //waypoint
+        var waypoint = new Waypoint({
+          element: document.getElementById('intro_guff'),
+          handler: function() {
+            $("#state_map").hide();
+        }
+      })
+
+
+        drawStateMap();
+
         // Initialize the map
         var map = new Landline.Stateline("#landline_container", "counties", options);
         
@@ -134,108 +145,144 @@ $(function() {
 
         //draw dots
         var width = 2400;
-var height = 2000;
-// D3 Projection
-var projection = d3.geo.albers()
-           .translate([width/2, height/2])    // translate to center of screen
-           .scale(10000);          // scale things down so see entire US
-        
-// Define path generator
-var path = d3.geo.path()               // path generator that will convert GeoJSON to SVG paths
-         .projection(projection);  // tell path generator to use albersUsa projection
+        var height = 2000;
+        // D3 Projection
+        var projection = d3.geo.albers()
+                   .translate([width/2, height/2])    // translate to center of screen
+                   .scale(10000);          // scale things down so see entire US
+                
+        // Define path generator
+        var path = d3.geo.path()               // path generator that will convert GeoJSON to SVG paths
+                 .projection(projection);  // tell path generator to use albersUsa projection
+            
+        // Define linear scale for output
+        var color = d3.scale.linear()
+                .range(['rgba(0,0,34,0)','rgba(0,0,34,0)']);
+        // var legendText = ["Cities Lived", "States Lived", "States Visited", "Nada"];
+        //Create SVG element and append map to the SVG
+        var svg = d3.select("#trauma_dot")
+              .append("svg")
+              .attr("width", width)
+              .attr("height", height);
+                
+        // Append Div for tooltip to SVG
+        var div = d3.select("#trauma_dot")
+                .append("div")   
+                .attr("class", "tooltip")               
+                .style("opacity", 0);
+      // Load in my states data!
+      d3.csv("data/2014state_fatalinjury.csv", function(data) {
+      color.domain([40,60,80,110]); // setting the range of the input data
+      // Load GeoJSON data and merge with states data
+      d3.json("data/us-states.json", function(json) {
+      // Loop through each state data value in the .csv file
+      for (var i = 0; i < data.length; i++) {
+        // Grab State Name
+        var dataState = data[i].state;
+        // Grab data value 
+        var dataValue = data[i].age_adjusted_rate;
+        // Find the corresponding state inside the GeoJSON
+        for (var j = 0; j < json.features.length; j++)  {
+          var jsonState = json.features[j].properties.name;
+          if (dataState == jsonState) {
+          // Copy the data value into the JSON
+          json.features[j].properties.age_adjusted_rate = dataValue; 
+          // Stop looking through the JSON
+          break;
+          }
+        }
+      }
     
-// Define linear scale for output
-var color = d3.scale.linear()
-        .range(['rgba(0,0,34,0)','rgba(0,0,34,0)']);
-// var legendText = ["Cities Lived", "States Lived", "States Visited", "Nada"];
-//Create SVG element and append map to the SVG
-var svg = d3.select("#trauma_dot")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height);
-        
-// Append Div for tooltip to SVG
-var div = d3.select("body")
-        .append("div")   
-        .attr("class", "tooltip")               
-        .style("opacity", 0);
-// Load in my states data!
-d3.csv("data/2014state_fatalinjury.csv", function(data) {
-color.domain([40,60,80,110]); // setting the range of the input data
-// Load GeoJSON data and merge with states data
-d3.json("data/us-states.json", function(json) {
-// Loop through each state data value in the .csv file
-for (var i = 0; i < data.length; i++) {
-  // Grab State Name
-  var dataState = data[i].state;
-  // Grab data value 
-  var dataValue = data[i].age_adjusted_rate;
-  // Find the corresponding state inside the GeoJSON
-  for (var j = 0; j < json.features.length; j++)  {
-    var jsonState = json.features[j].properties.name;
-    if (dataState == jsonState) {
-    // Copy the data value into the JSON
-    json.features[j].properties.age_adjusted_rate = dataValue; 
-    // Stop looking through the JSON
-    break;
-    }
-  }
-}
-    
-// Bind the data to the SVG and create one path per GeoJSON feature
-svg.selectAll("path")
-  .data(json.features)
-  .enter()
-  .append("path")
-  .attr("d", path)
-  .style("stroke", "#fff")
-  .style("stroke-width", "1")
-  .style("fill", function(d) {
-  // Get data value
-  var value = d.properties.age_adjusted_rate;
-  if (value) {
-  //If value exists…
-  return color(value);
-  } else {
-  //If value is undefined…
-  return "rgb(213,222,217)";
-  }
-});
-     
-d3.csv("data/traumacenterlist_ok.csv", function(data) {
-svg.selectAll("circle")
-  .data(data)
-  .enter()
-  .append("circle")
-    .filter(function(d) { return d.level == 1 || d.level == 2;})
-  .attr("cx", function(d) {
-    return projection([d.lon, d.lat])[0];
-  })
-  .attr("cy", function(d) {
-    return projection([d.lon, d.lat])[1];
-  })
-  .attr("r", function(d) {
-    return 5;
-  })
-    .style("fill", "rgb(217,91,67)")  
-    .style("opacity", 0.85) 
-  // Modification of custom tooltip code provided by Malcolm Maclean, "D3 Tips and Tricks" 
-  // http://www.d3noob.org/2013/01/adding-tooltips-to-d3js-graph.html
-  .on("mouseover", function(d) {      
-      div.transition()        
-           .duration(200)      
-           .style("opacity", .9);      
-           div.text(d.place)
-           .style("left", (d3.event.pageX) + "px")     
-           .style("top", (d3.event.pageY - 28) + "px");    
-  })   
-    // fade out tooltip on mouse out               
-    .on("mouseout", function(d) {       
-        div.transition()        
-           .duration(500)      
-           .style("opacity", 0);   
+    // Bind the data to the SVG and create one path per GeoJSON feature
+    svg.selectAll("path")
+      .data(json.features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .style("stroke", "#fff")
+      .style("stroke-width", "1")
+      .style("fill", function(d) {
+      // Get data value
+      var value = d.properties.age_adjusted_rate;
+      if (value) {
+      //If value exists…
+      return color(value);
+      } else {
+      //If value is undefined…
+      return "rgb(213,222,217)";
+      }
     });
-});  
+
+
+    d3.csv("data/example.csv", function(data) {
+    svg.selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cx", function(d) {
+        return projection([d.lon, d.lat])[0];
+      })
+      .attr("cy", function(d) {
+        return projection([d.lon, d.lat])[1];
+      })
+      .attr("r", function(d) {
+        return 10;
+      })
+        .style("fill", "rgb(217,91,67)")  
+        .style("opacity", 0.85) 
+      // Modification of custom tooltip code provided by Malcolm Maclean, "D3 Tips and Tricks" 
+      // http://www.d3noob.org/2013/01/adding-tooltips-to-d3js-graph.html
+      .on("mouseover", function(d) {      
+          div.transition()        
+               .duration(200)      
+               .style("opacity", .9);      
+               div.text(d.place)
+               .style("left", (d3.event.pageX) + "px")     
+               .style("top", (d3.event.pageY - 28) + "px");    
+      })   
+        // fade out tooltip on mouse out               
+        .on("mouseout", function(d) {       
+            div.transition()        
+               .duration(500)      
+               .style("opacity", 0);   
+        });
+    });  
+     
+     
+    d3.csv("data/traumacenterlist_ok.csv", function(data) {
+    svg.selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .filter(function(d) { return d.level == 1 || d.level == 2;})
+      .attr("cx", function(d) {
+        return projection([d.lon, d.lat])[0];
+      })
+      .attr("cy", function(d) {
+        return projection([d.lon, d.lat])[1];
+      })
+      .attr("r", function(d) {
+        return 5;
+      })
+        .style("fill", "rgb(217,91,67)")  
+        .style("opacity", 0.85) 
+      // Modification of custom tooltip code provided by Malcolm Maclean, "D3 Tips and Tricks" 
+      // http://www.d3noob.org/2013/01/adding-tooltips-to-d3js-graph.html
+      .on("mouseover", function(d) {      
+          div.transition()        
+               .duration(200)      
+               .style("opacity", .9);      
+               div.text(d.place)
+               .style("left", (d3.event.pageX) + "px")     
+               .style("top", (d3.event.pageY - 28) + "px");    
+      })   
+        // fade out tooltip on mouse out               
+        .on("mouseout", function(d) {       
+            div.transition()        
+               .duration(500)      
+               .style("opacity", 0);   
+        });
+    });  
 
 
         
